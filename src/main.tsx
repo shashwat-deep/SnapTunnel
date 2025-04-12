40cdc7c7cc7ed487e6304c702c88360886a307e9
Ayush Ranjan <ayush82194@gmail.com>
2025-04-12 16:16:26 +0530

    changes in App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container missing in index.html');
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
