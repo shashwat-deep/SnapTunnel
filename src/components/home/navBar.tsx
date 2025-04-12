4bd3b75056d34d2c273bbeb9933f638fbdb3a985
Ayush Ranjan <ayush82194@gmail.com>
2025-04-12 17:25:29 +0530

    Added google material icons in NavBar

--- src/components/home/navBar.tsx
+++ src/components/home/navBar.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './navBar.css';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/" className="navbar-link">
-          <img src="/tatva-icon.svg" alt="TatvaCare Logo" className="logo-img" />
+          {/* <img src="/tatva-icon.svg" alt="Logo" className="logo-img" /> */}
           <span className="logo-text">Snaptunnel</span>
         </Link>
       </div>
       <ul className="navbar-menu">
-        <li>
-            <Link to="/room" className="navbar-link">Create Room</Link>
-        </li>
-        <li>
-            <Link to="/placeholder" className="navbar-link">Placeholder</Link>
-        </li>
+        <li className="navbar-link"><a href='#about'>About</a></li>
+        <li className="navbar-link"><a href='#pricing'>Pricing</a></li>
       </ul>
+      <div>
+        <div className="">
+          <span className="material-symbols-outlined">help</span>
+          <span className="material-symbols-outlined">settings</span>
+        </div>
+        <div className='account'>
+          <span className="material-symbols-outlined">account_circle</span>
+        </div>
+      </div>
+
     </nav>
   );
 };
